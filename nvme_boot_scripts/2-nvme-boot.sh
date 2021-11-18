e0b8d33f0366ce30cdf42aed58f718b11e8d8dd4
tensorturtle <tensorturtle@gmail.com>
2021-11-19 04:50:50 +0900

    new

--- nvme_boot_scripts/2-nvme-boot.sh
+++ nvme_boot_scripts/2-nvme-boot.sh
@@ -4,7 +4,7 @@ cd ~
 
 read -p "If NVME drive is connected, format it so that there is one ext4 partition and 16GB of free space. Enter any key when done." NVME
 
-git clone https://github.com/jetsonhacks/rootOnNVMe.git
+git clone https://github.com/hydoai/rootOnNVMe.git
 cd rootOnNVMe
 ./copy-rootfs-ssd.sh
 ./setup-service.sh
